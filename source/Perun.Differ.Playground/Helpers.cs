60ba4d5f46aa6a357327337f34419d6a6e4d90f0
Marko Urh <urh.marko@gmail.com>
2024-08-22 00:34:26 +0200

    minor: add playground console

---
+++ source/Perun.Differ.Playground/Helpers.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using Differ.DotNet;
+
+public static class DifferenceExtensions
+{
+    public static void Output(this IEnumerable<Difference> diffs)
+    {
+        Console.WriteLine(JsonSerializer.Serialize(diffs, new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+        }));
+    }
+}
---
+++ source/Perun.Differ.Playground/Program.cs
@@ -0,0 +1,9 @@
+﻿using Differ.DotNet;
+
+var car1 = new Car("Toyota Camry", "Blue", 2022);
+var car2 = new Car("Ford Mustang", "Red", 2023);
+
+var carDiff = DifferDotNet.Diff(car1, car2);
+carDiff.Output();
+
+record Car(string Model, string Color, int Year);
